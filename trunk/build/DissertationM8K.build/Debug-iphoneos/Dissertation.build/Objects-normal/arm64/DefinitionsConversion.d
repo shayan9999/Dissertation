064e901802fe37108afad223669d528f682288db
shayan9999 <shayan9999@gmail.com>
2016-07-18 00:32:23 +0100

    Adding build folder to .gitignore
    Had to remove files accidentally added before

--- trunk/build/DissertationM8K.build/Debug-iphoneos/Dissertation.build/Objects-normal/arm64/DefinitionsConversion.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/macbook/Workspace/Dissertation/trunk/DissertationM8K/Frameworks/Estimote/DefinitionsConversion.m \
-  /Users/macbook/Workspace/Dissertation/trunk/DissertationM8K/Frameworks/Estimote/DefinitionsConversion.h \
-  /Users/macbook/Workspace/Dissertation/trunk/DissertationM8K/Frameworks/Estimote/EstimoteSDK.framework/Headers/ESTBeaconDefinitions.h \
-  /Users/macbook/Workspace/Dissertation/trunk/DissertationM8K/Frameworks/Estimote/EstimoteSDK.framework/Headers/ESTDefinitions.h
--- trunk/build/DissertationM8K.build/Debug-iphonesimulator/DissertationM8K.build/Script-AED499A41BDA532C00445E08.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-tags="TODO:"
-matches=`find "${SRCROOT}" \( -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($tags).*\$"`
-echo "$matches" | perl -p -e "s/($tags)/ error: \$1/"
-[ -z "$matches" ] || exit 1
