662171c6a926615a82bebc3fc56c3af1036d7346
shayan9999 <shayan9999@gmail.com>
2016-08-28 01:46:54 +0100

    Adding Encouragements support for Caretaker - Phase 1
    Added //TODO for the tasks to implements

---
+++ trunk/DissertationM8K/CaretakerApp/ViewControllers/EncouragementsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EncouragementsViewController.swift
+//  DissertationM8K
+//
+//  Created by Shayan K. on 8/28/16.
+//  Copyright © 2016 Orchard. All rights reserved.
+//
+
+import Foundation
+
+class EncouragementsViewController: UIViewController{
+    
+    //TODO: Use this viewController to create new Encouragements for the elderly
+    // Add those Encouragements to the iCloud database 
+    // Show multiple TIMING options on the view with: Name, Date (Time)
+    
+}
